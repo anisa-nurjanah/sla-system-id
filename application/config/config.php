9391cb576a67c47857027c1d674b7927d6f3e6d8
civokyuu <anisa_nurjanah@apps.ipb.ac.id>
2021-06-02 11:37:09 +0700

    first commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sla-system/';
+$config['base_url'] = 'http://localhost/sla-system-id/';
 
 /*
 |--------------------------------------------------------------------------
