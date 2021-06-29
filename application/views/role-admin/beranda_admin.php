ef67386dcd2826bcc70c56aca595104383f3db29
civokyuu <anisa_nurjanah@apps.ipb.ac.id>
2021-06-29 12:01:34 +0700

    ngebenerin template

--- application/views/role-admin/beranda_admin.php
+++ application/views/role-admin/beranda_admin.php
@@ -5,29 +5,64 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Beranda Admin</h1>
-          </div><!-- /.col -->
+            <h1 class="m-0">Dashboard Admin</h1>
+          </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <!-- <li class="breadcrumb-item"><a href="#">Home</a></li> -->
-              <li class="breadcrumb-item active">Beranda Admin</li>
+              <li class="breadcrumb-item active">Admin</li>
             </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-  
-  <!-- Main content -->
-  <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="card-footer clearfix">
-            <a href="<?php echo site_url('Beranda/insert');?>" class="btn btn-primary">Tambah User</a>
           </div>
-          <!-- ./col -->
         </div>
       </div>
-  </section>
+    </div>
+  
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-orange">
+              <div class="card-header">
+                <h3 class="card-title">Formulir Pengguna</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form>
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">NPP</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nomor PAK">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Nama</label>
+                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nama PAK">
+                  </div>
+                  <div class="form-group">
+                  <label>Posisi</label>
+                  <div class="select2-purple">
+                  <select class="form-control select2bs4" style="width: 100%;">
+                      <option>Admin</option>
+                      <option>Sekretaris Komite</option>
+                      <option>Sekretaris</option>
+                      <option>Sekretaris Bisnis</option>
+                      <option>Pemimpin</option>
+                      <option>DGM/GM</option>
+                      <option>Pengelola</option>
+                      <option>Analyst</option>
+                    </select>
+                  </div>
+                  </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card -->
 </div>
+</div>
+</div>
+</section>
